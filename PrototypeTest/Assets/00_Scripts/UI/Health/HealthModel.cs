1524e9cb739c0c778277eee619e80c39b903efaf
Yoo-JaeKwang <jaeu4100@gmail.com>
2023-05-24 17:55:34 +0900

    [Chore] 헤더 프리펩으로 변경

--- PrototypeTest/Assets/00_Scripts/UI/Health/HealthModel.cs
+++ PrototypeTest/Assets/00_Scripts/UI/Health/HealthModel.cs
@@ -4,6 +4,9 @@ public static class HealthModel
 {
     public static readonly ReactiveProperty<int> MaxHealth = new();
     public static readonly ReactiveProperty<int> CurHealth = new();
+    //sp
+    //잔탄
+    //레벨
     public static void SetMaxHealth(int maxHealth)
     {
         MaxHealth.Value = maxHealth;
